5bc0e83afcfbfdddb3115b5f2914d9778c65908e
corsacca <corsacca@gmail.com>
2021-06-04 09:48:31 -0500

    Update dt_get_url_path function

--- includes/functions.php
+++ includes/functions.php
@@ -201,13 +201,17 @@ if ( ! is_this_dt() ) {
     }
 
     if ( ! function_exists( 'dt_get_url_path' ) ) {
-        function dt_get_url_path() {
+        function dt_get_url_path( $ignore_query_parameters = false ) {
             if ( isset( $_SERVER["HTTP_HOST"] ) ) {
                 $url  = ( !isset( $_SERVER["HTTPS"] ) || @( $_SERVER["HTTPS"] != 'on' ) ) ? 'http://'. sanitize_text_field( wp_unslash( $_SERVER["HTTP_HOST"] ) ) : 'https://'. sanitize_text_field( wp_unslash( $_SERVER["HTTP_HOST"] ) );
                 if ( isset( $_SERVER["REQUEST_URI"] ) ) {
                     $url .= sanitize_text_field( wp_unslash( $_SERVER["REQUEST_URI"] ) );
                 }
-                return trim( str_replace( get_site_url(), "", $url ), '/' );
+                $url = trim( str_replace( get_site_url(), "", $url ), '/' );
+                if ( $ignore_query_parameters ){
+                    return strtok( $url, '?' ); //allow get parameters
+                }
+                return $url;
             }
             return '';
         }

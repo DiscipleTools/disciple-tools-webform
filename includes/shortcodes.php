79ebd2bb42fbda9582d286a1f95e6e3459742511
kodinkat <me@karlkatewu.com>
2022-01-21 08:21:17 +0000

    Refactored form html generation logic

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,6 +1,5 @@
 <?php
 
-
 add_action( 'after_setup_theme', 'register_webform_shortcodes' );
 function register_webform_shortcodes() {
     add_shortcode( 'dt-webform', 'webform_shortcodes_handler' );
@@ -26,85 +25,35 @@ function webform_shortcodes_handler( $atts ): string {
             return $public_url;
 
         } else {
-            $response = wp_remote_get( $public_url, [
-                'headers' => [
-                    'Content-Type' => 'text/html'
-                ]
-            ] );
 
-            // If valid, cheery-pick html elements of interest
-            if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+            // Call required helper classes
+            require_once( 'utilities.php' );
+            require_once( 'post-type-active-forms.php' );
 
-                // Convert to dom and extract elements of interest
-                $dom = new DOMDocument();
-                @$dom->loadHTML( $response['body'] );
+            // Determine meta and associated form fields
+            $dt_webform_meta        = DT_Webform_Utilities::get_form_meta( $token );
+            $dt_webform_core_fields = DT_Webform_Active_Form_Post_Type::get_core_fields_by_token( $token );
+            $dt_webform_fields      = DT_Webform_Active_Form_Post_Type::get_extra_fields( $token );
 
-                // Process any referenced scripts and capture embedded ones!
-                $embedded_scripts = webform_shortcodes_elements( $dom, 'script' );
+            // Generate html to be returned
+            $form_html    = DT_Webform_Utilities::get_form_html( $token, $dt_webform_core_fields, $dt_webform_fields );
+            $scripts_html = DT_Webform_Utilities::get_form_html_scripts_and_styles( $token, $dt_webform_meta, $dt_webform_fields );
 
-                // Capture identified styles to be embedded
-                $embedded_styles = webform_shortcodes_elements( $dom, 'style' );
+            // Generate html to be returned
+            return "
+            <div id=\"wrapper\">
+                <form id=\"contact-form\" action=\"\">
+                    {$form_html}
+                </form>
 
-                /**
-                 * Concatenate existing form, along with any identified embedded scripts and styles.
-                 * Ensure to include required divs!
-                 */
+                <div id=\"report\"></div>
+                <div id=\"offlineWarningContainer\"></div>
 
-                $html = $dom->saveHTML( $dom->getElementById( 'wrapper' ) );
-
-                /*$html = '<div id="wrapper">';
-                $html .= $dom->saveHTML( $dom->getElementsByTagName( 'form' )->item( 0 ) );
-                $html .= '<div id="report"></div>';
-                $html .= '<div id="offlineWarningContainer"></div>';
-                $html .= '</div>';*/
-
-                $html .= ! empty( $embedded_scripts ) ? implode( ' ', $embedded_scripts ) : '';
-                $html .= ! empty( $embedded_styles ) ? implode( ' ', $embedded_styles ) : '';
-
-                return $html;
-            }
+                {$scripts_html}
+            </div>
+            ";
         }
     }
 
     return '';
-}
-
-function webform_shortcodes_elements( DOMDocument $dom, $name ): array {
-    $elements = [];
-    foreach ( $dom->getElementsByTagName( $name ) as $element ) {
-        $elements[] = $dom->saveHTML( $element );
-    }
-
-    return $elements;
-}
-
-
-function webform_shortcodes_scripts( DOMDocument $dom ): array {
-    $embedded_scripts = [];
-    foreach ( $dom->getElementsByTagName( 'script' ) as $script ) {
-
-        // Differentiate between referenced scripts and embedded
-        if ( ! empty( $script->attributes->getNamedItem( 'src' ) ) ) {
-
-            // Dynamically generate wp enqueue script for referenced files
-            $src = $script->attributes->getNamedItem( 'src' );
-            wp_enqueue_script( time(), $src->value );
-
-        } else {
-
-            // Capture embedded scripts to be returned with form
-            $embedded_scripts[] = $dom->saveHTML( $script );
-        }
-    }
-
-    return $embedded_scripts;
-}
-
-function webform_shortcodes_styles( DOMDocument $dom ): array {
-    $embedded_styles = [];
-    foreach ( $dom->getElementsByTagName( 'style' ) as $style ) {
-        $embedded_styles[] = $dom->saveHTML( $style );
-    }
-
-    return $embedded_styles;
 }

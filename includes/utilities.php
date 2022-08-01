cd87046747e11e9f2f4143a9f15391d4738f8b96
corsacca <corsacca@gmail.com>
2022-08-01 18:40:09 +0100

    Let remote access field list even if sources field not present.

--- includes/utilities.php
+++ includes/utilities.php
@@ -120,7 +120,7 @@ class DT_Webform_Utilities {
         }
 
         $contact_defaults = json_decode( $result['body'], true );
-        if ( isset( $contact_defaults["fields"]['sources'] ) ) {
+        if ( isset( $contact_defaults["fields"] ) ) {
             set_transient( 'dt_webform_contact_defaults', $contact_defaults["fields"], DAY_IN_SECONDS );
             return $contact_defaults["fields"];
         } else {

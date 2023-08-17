8555d18665a3133102854e247698faa5352a86e3
corsacca <corsacca@gmail.com>
2023-08-17 14:00:49 +0100

    Better enqueue

--- includes/utilities.php
+++ includes/utilities.php
@@ -968,11 +968,13 @@ class DT_Webform_Utilities {
             ?>
             <script type="text/javascript" src="<?php echo esc_url( $public_url ) ?>jquery.min.js?v=1"></script>
             <script type="text/javascript" src="<?php echo esc_url( $public_url ) ?>jquery-migrate.min.js"></script>
+            <script type="text/javascript" src="<?php echo esc_url( $public_url ) ?>jquery.validate.min.js"></script>
             <?php
+        } else {
+            wp_enqueue_script( 'jquery-validate' , $public_url . 'jquery.validate.min.js', array( 'jquery' ), '1.17.0', true );
         }
         ?>
 
-        <script type="text/javascript" src="<?php echo esc_url( $public_url ) ?>jquery.validate.min.js"></script>
         <script type="text/javascript"
                 src="<?php echo esc_url( $public_url ) ?>public.js?ver=<?php echo esc_html( filemtime( plugin_dir_path( __DIR__ ) . 'public/public.js' ) ) ?>"></script>
         <?php if( get_option( 'dt_google_map_key' ) ) { ?>
